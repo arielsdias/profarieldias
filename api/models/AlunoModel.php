7bc8392f536cfa6ac6c6e7d04e05afc9d982269b
Ariel Dias <arielsdias@gmail.com>
2025-12-09 14:07:58 -0300

    modelos

--- api/models/AlunoModel.php
+++ api/models/AlunoModel.php
@@ -1,10 +1,53 @@
 <?php
-class AlunoModel {
 
-    public static function buscarPorEmail($email) {
-        $db = Database::getConnection();
-        $stmt = $db->prepare("SELECT * FROM aluno WHERE email = ?");
-        $stmt->execute([$email]);
-        return $stmt->fetch();
+require_once __DIR__ . '/../core/Database.php';
+
+class AlunoModel
+{
+    private $db;
+
+    public function __construct()
+    {
+        $this->db = Database::getInstance();
+    }
+
+    public function getAll()
+    {
+        $sql = "SELECT id_aluno, nome, email, data_cadastro FROM aluno";
+        return $this->db->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public function getById($id)
+    {
+        $sql = "SELECT id_aluno, nome, email, data_cadastro FROM aluno WHERE id_aluno = ?";
+        $stm = $this->db->prepare($sql);
+        $stm->execute([$id]);
+        return $stm->fetch(PDO::FETCH_ASSOC);
+    }
+
+    public function insert($nome, $email, $senhaHash)
+    {
+        $sql = "INSERT INTO aluno (nome, email, senha_hash) VALUES (?, ?, ?)";
+        $stm = $this->db->prepare($sql);
+        $stm->execute([$nome, $email, $senhaHash]);
+        return $this->db->lastInsertId();
+    }
+
+    public function update($id, $dados)
+    {
+        $sql = "UPDATE aluno SET nome = ?, email = ? WHERE id_aluno = ?";
+        $stm = $this->db->prepare($sql);
+        $stm->execute([
+            $dados["nome"] ?? null,
+            $dados["email"] ?? null,
+            $id
+        ]);
+    }
+
+    public function delete($id)
+    {
+        $sql = "DELETE FROM aluno WHERE id_aluno = ?";
+        $stm = $this->db->prepare($sql);
+        $stm->execute([$id]);
     }
 }
